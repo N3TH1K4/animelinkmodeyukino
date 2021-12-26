356fdebe3800028f82dafa5e96baafdab88b62a0
Trojen1 <88619598+Trojen1@users.noreply.github.com>
2021-12-26 09:49:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import requests
+import link
 #"https://source.unsplash.com/1600x900/?{keyword},{keyword}"
 from telethon.sync import TelegramClient ,events,Button
 api_id = 4091096
@@ -8,7 +9,6 @@ bot_token = '5069583926:AAFl9BFiu6ZyMOPHXxLQkWvkhjp46fPha3Y'
 # We have to manually call "start" if we want an explicit bot token
 bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 
-op=[Button.url('Naruto','https://t.me/Naruto_and_NarutoShippuden')]
 
 with bot:
     @bot.on(events.NewMessage(pattern="^/anime (.*)"))
