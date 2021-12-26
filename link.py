2bb318debeb31f14c3e17297d3265a1bd9a7920c
Trojen1 <88619598+Trojen1@users.noreply.github.com>
2021-12-26 10:33:23 +0530

    Update link.py

--- link.py
+++ link.py
@@ -1,4 +1,19 @@
 from telethon.sync import TelegramClient ,events,Button
 
 
-op=[Button.url('Naruto','https://t.me/Naruto_and_NarutoShippuden')]
+naruto=[Button.url('Naruto','https://t.me/Naruto_and_NarutoShippuden')]
+placefur=[Button.url('A Place Further Than The Universe','http://t.me/APlaceFurtherThanTheUniverse')]
+slientv=[Button.url('A Silent Voice','https://t.me/A_Silent_Voice_Koe_no_Katachi')]
+whiskera=[Button.url('A Whisker Away','https://t.me/Whisker_Away')]
+afterrain=[Button.url('After The Rain','https://t.me/After_The_Rain_Series')]
+ahiruno=[Button.url('Ahiru No Sora','http://t.me/Ahiru_No_Sora_Series')]
+ajin=[Button.url('Ajin','https://t.me/AjinDemiHuman')]
+akame=[Button.url('Akame Ga Kill','http://t.me/Akame_Ga_Kill_Dual')]
+akira=[Button.url('Akira','https://t.me/Akira_AnimeMovie')]
+akudama=[Button.url('Akudama Drive','http://t.me/Akudama_Drive_Series')]
+angel=[Button.url('Angel Beats','https://t.me/Angel_Beats_Series')]
+anohana=[Button.url('Anohana','http://t.me/Anohana_Dual')]
+another=[Button.url('Another','https://t.me/Another_Dual')]
+aoi=[Button.url('Aoi Bungaku Series','http://t.me/AoiBungakuSeries')]
+naruto=[Button.url('Naruto','https://t.me/Naruto_and_NarutoShippuden')]
+naruto=[Button.url('Naruto','https://t.me/Naruto_and_NarutoShippuden')]
