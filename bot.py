bcb1fe47c0633c183fef5144b045851fc2d299ef
N3TH1K4 <88619598+N3TH1K4@users.noreply.github.com>
2022-01-15 21:13:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ with bot:
     async def imgd(event):
         page = requests.get("https://waifunet.000webhostapp.com/meme.html")
         souped = BeautifulSoup(page.content, "html.parser")
-        imgs = souped.find_all("img id")
+        imgs = souped.find_all("image")
         for img in imgs:
             imglink = img.attrs.get("src")
             print(imglink)
