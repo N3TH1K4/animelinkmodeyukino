a9a989b5b3863b057e52b698a5d7e60a3c156b20
N3TH1K4 <88619598+N3TH1K4@users.noreply.github.com>
2022-01-15 21:38:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,17 +11,8 @@ bot_token = '2025919134:AAGAyAXR9hTJZu6v75-5ho8ao95mcppXacU'
 bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 txt = '🤍 Choose Any Button To Get The Animes That Can Download Using /anime <name> 🤍'
 dwn = 'Is This The One You Were Searching? Here Some Download Links According To The Searched Query 🤍👇'
-print("helo")
+print("Bot Started Successfully!")
 with bot:
-    @bot.on(events.NewMessage(pattern="^/meme"))
-    async def imgd(event):
-        a_string = soup.find_all(string="img")
-        page = requests.get("https://waifunet.000webhostapp.com/meme.html")
-        souped = BeautifulSoup(page.content, "html.parser")
-        imgs = a_string.find_parents("src")
-        for img in imgs:
-            imglink = img.attrs.get("src")
-            print(imglink)
         
         
         
