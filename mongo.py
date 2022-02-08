8125238463d830cd8a411dcf5c85b0719a022d1c
N3TH1K4 <88619598+N3TH1K4@users.noreply.github.com>
2022-02-08 16:45:19 +0530

    Update mongo.py

--- mongo.py
+++ mongo.py
@@ -13,22 +13,14 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import asyncio
 
-from motor import motor_asyncio
-from odmantic import AIOEngine
-from pymongo import MongoClient
-from pymongo.errors import ServerSelectionTimeoutError
+from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 
+MONGO2 = "mongodb+srv://TROJ3N:Nethika123@cluster0.uppg6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+MONGO = "mongodb+srv://TROJ3N:Nethika123@cluster0.uppg6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+if MONGO2 == None:
+    MONGO2 = MONGO
 
-MONGO_URI = "mongodb+srv://TROJ3N:Nethika123@cluster0.uppg6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-MONGO_PORT = 27017
-MONGO_DB = "DaisyX"
-
-# Init MongoDB
-mongodb = MongoClient(MONGO_URI, MONGO_PORT)[MONGO_DB]
-motor = motor_asyncio.AsyncIOMotorClient(MONGO_URI, MONGO_PORT)
-db = motor[MONGO_DB]
-
-engine = AIOEngine(motor, MONGO_DB)
+mongo_client = MongoClient(MONGO2)
+db = mongo_client.daisy
 
