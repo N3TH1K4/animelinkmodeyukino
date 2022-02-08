f0ade2fa3b54aac1c898b910bd06285a39595531
N3TH1K4 <88619598+N3TH1K4@users.noreply.github.com>
2022-02-08 15:37:02 +0530

    Update manga.py

--- manga.py
+++ manga.py
@@ -4,13 +4,12 @@ from telethon.sync import TelegramClient ,events
 from telethon.sessions import StringSession
 api_id = 4091096
 api_hash = '6bb0682b4af56456201c3b9d8b99c94a'
-bot_token = '2025919134:AAEiMUp8gZ3W2QD-YAgDpGZXcYol_RfEPWg'
+bot_token = '2025919134:AAEtaYWwSrYcIQsK7-1zn5aA581bb0hDJNc'
 STRING_SESSION = "1BVtsOK0BuyOrsOmanqjqtRki98zGYMZtJE0CoaDRuBmlbw-bGzviSFhA3KhgMnKtUxyr9EDwgswl2ddFqptdD0NbBKRLCspOw9sJFtaIpj6A1XpCcC4d1cgddpROD4zXO2oYUwycDU2qvmyeyetRMwfo6J7-mCji1x66qQX_FWKQ5gkKv1ZY7W0XIf-0jpvJU60e2pGA_pY398QKmGdyK_soCsLVbclgs7QgJPw67-T4ManKid3qGCNUw3BHM8elvebCEBlHdt7IuJy2UmQx2hjStKZpuD0bAarI28yDCV6R1z33VTbJFmVTf-rSG5Ct33uNkva7SDyXnkQo3zcnagdjeWlu30A="
 # We have to manually call "start" if we want an explicit bot token
 tbot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 ubot = TelegramClient(StringSession(STRING_SESSION), api_id, api_hash)
-try:
-    ubot.start()
+
 
 @tbot.on(events.NewMessage(pattern="^/cc (.*)"))
 async def alive(event):
