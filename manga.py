e04daae58bece15e0d00064fe6e7b540108c6a4a
N3TH1K4 <88619598+N3TH1K4@users.noreply.github.com>
2022-02-08 18:20:34 +0530

    Update manga.py

--- manga.py
+++ manga.py
@@ -29,7 +29,7 @@ async def alisve(event):
         await bot_conv.send_message("/read " +ok)
         await asyncio.sleep(5)
         response = await bot_conv.get_response()
-        file= response.stringify()
+        file= response.link_preview=True
         await m.edit(file)
         
 ubot.start()
